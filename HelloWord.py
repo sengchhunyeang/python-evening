05c43398fc581ab0c4c15de6d74d47b421fa2974
chhunyeangseng <chhunyeangseng@gmail.com>
2025-04-27 13:40:39 +0700

    First commit

---
+++ HelloWord.py
@@ -0,0 +1,33 @@
+
+
+# This is print hello word 
+# Single is show developer write hello word 
+
+
+# what is multiple cmt 
+'''
+if is comment multi line we use multiple comment 
+'''
+# print("Hello word ")
+
+# How to declare 
+a = 5 
+# a is variable 
+# = Assignt 
+# value (String and number "flot int ...")
+
+# Datatype 
+# What is Datatype 
+
+
+# user input ??
+
+name = input("Input your name  :")
+age = input( "input your age : ")
+gender = input("input your gender :")
+
+print("================Show information ===================")
+
+print("Name :" ,name)
+print("age :",age)
+print("gender :",gender)
---
+++ datatype.py
@@ -0,0 +1,27 @@
+# what is datatype 
+# String is stored data as text 
+# How to daclare string 
+myString = "only text 2025"
+print(myString)
+
+# Integer is number 0 - 9
+# How to declare integer 
+myInt = 7
+myValue = 3
+print(myInt)
+print(myValue)
+
+
+
+# 1 what is string ??
+# 2 what is integer ?
+
+numberA= int(input("numberA: "))
+numberB = int(input("numberB :"))
+
+
+print("=================Answer==============")
+print(numberA ,"-",numberA ,"=",numberA-numberB)
+print(numberA ,"+",numberA ,"=",numberA+numberB)
+print(numberA ,"*",numberA ,"=",numberA*numberB)
+print(numberA ,"%",numberA ,"=",numberA/numberB)
