b57ad83a9321a1cc7c0dd8d6cffd76215e081c52
chhunyeang <chhunyeangseng@gmail.com>
2025-07-04 18:27:11 +0700

    homework python mon-wed

---
+++ python-monday-tuesday-wednesday/first/list/Home_Work.py
@@ -0,0 +1,7 @@
+# Homework
+# favorite_colors = ['red', 'blue', 'green', 'yellow', 'purple']
+# 1-Create a list called favorite_colors that contains at least 5 colors.
+# 2-Print the first and last color from the favorite_colors list.
+# 3-Change the second color in the list to "black".
+# 4-Insert "orange" as the third item in the list.
+# 5-Remove "yellow" from the list using .remove().
