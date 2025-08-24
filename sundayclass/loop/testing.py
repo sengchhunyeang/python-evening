598eded40e872cf1e88aeb4064a672dab2706f0d
chhunyeang <chhunyeangseng@gmail.com>
2025-08-24 10:19:22 +0700

    ada

---
+++ sundayclass/loop/testing.py
@@ -0,0 +1,11 @@
+# practice 
+# loop 1 to 10 using for loop 
+
+# practice 2 
+list = ["monday","Tuesday","Wenesday","Thursday","friday","saturday","sunday"]
+# using for loop item list  
+
+
+# practice 3 
+list = ["monday","Tuesday","Wenesday","Thursday","friday","saturday","sunday"]
+# print only suday using for and condition
