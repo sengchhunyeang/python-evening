2804c3fb2318184a923cde3c7b8a922b84628d3a
chhunyeang <chhunyeangseng@gmail.com>
2025-08-26 19:46:06 +0700

    final project add_user and view_user

---
+++ python-monday-tuesday-wednesday/Final_project/User.py
@@ -0,0 +1,7 @@
+# create class user
+class User:
+    def __init__(self,user_id,user_name,user_email):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.user_email = user_email
+
---
+++ python-monday-tuesday-wednesday/OOP/ravid/page/Store.py
@@ -0,0 +1,15 @@
+
+
+cafe = {
+  "id": "1",
+  "name": "H1",
+  "price": 200
+}
+
+dessert = {
+  "id": "1",
+  "name": "D1",
+  "price":456
+}
+
+
