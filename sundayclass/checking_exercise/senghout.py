9f5bd5a2672e762e87daa62067dd38db5010f065
chhunyeang1 <gak2978@gmail.com>
2025-08-26 16:43:38 +0700

    dsds

---
+++ sundayclass/checking_exercise/senghout.py
@@ -0,0 +1,25 @@
+Name="sokha"
+Age=45
+Sex="Male"
+Children=2
+Place="Phnom Penh"
+School="ABC university"
+Career="Engineer"
+a=100
+b=102
+c=1
+# print("His name is :",Name,",",Sex)
+# print("He is",Age)
+# print("He has",Children,"Children")
+# print("He is living in",Place,"and work as an",Career)
+# print("He studied at",School)
+#first way
+print("His name is :",Name,",",Sex,"\nHe is ",Age,"\nHe has",Children,"Children","\nHe is living in",Place,"and work as an",Career,"\nHe studied at",School)
+#second way
+print("His name is :"+Name+","+Sex+"\nHe is",Age,"\nHe has",Children,"Children","\nHe is living in",Place,"and work as an",Career,"\nHe studied at",School)
+#Third way
+print(f"His name is :{Name},{Sex}\nHe is {Age}\nHe has {Children} Children\nHe is living in{Place}and work as an{Career}\nHe studied at{School}")
+#boolean
+print(a>b)
+print(a<b)
+print(a+c<b)
